4f955af479dddebe855efb43c00b1cb69f78651c
Obrienmaina <brianmaina.nyawira@gmail.com>
2025-08-29 07:18:30 +0200

    delete disfunctional admin page

--- jasiricup/src/app/admin/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import AdminBlogManager from "@/components/admin/AdminBlogManager";
-
-export default function AdminDashboardPage() {
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <AdminBlogManager />
-    </div>
-  );
-}
--- jasiricup/src/components/admin/hooks/useAdminAuth.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react';
-
-const useAdminAuth = () => {
-  const [adminToken, setAdminToken] = useState('');
-
-  return { adminToken, setAdminToken };
-};
-
-export default useAdminAuth;
